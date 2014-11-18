f22bef8b9bba03632da22fa3e961a2eaafaa0c38
Siddharth Sathyam <ssathy2@gmail.com>
2014-11-17 19:40:40 -0600

    Initial Commit

---
+++ Hakken/Hakken/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hakken
+//
+//  Created by Sidd Sathyam on 11/17/14.
+//  Copyright (c) 2014 dotdotdot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
