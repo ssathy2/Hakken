3a46b4410f33a9281336e61b504be994867ddbf2
Siddharth Sathyam <ssathy2@gmail.com>
2014-11-17 19:50:01 -0600

    Setting up dependencies (cocoapods), added base view controller and view model class

---
+++ Hakken/Hakken/View Controllers/DDDViewController.m
@@ -0,0 +1,71 @@
+//
+//  DDDBaseViewController.m
+//  autolayouttest
+//
+//  Created by Sidd Sathyam on 02/05/14.
+//  Copyright (c) 2014 dotdotdot. All rights reserved.
+//
+
+#import "DDDViewController.h"
+
+@interface DDDViewController ()<DDDViewModelListener>
+@end
+
+@implementation DDDViewController
++ (instancetype)instance
+{
+	NSString *storyboardName = [self storyboardName];
+	UIStoryboard *sb = [UIStoryboard storyboardWithName:storyboardName bundle:[NSBundle mainBundle]];
+	return [sb instantiateViewControllerWithIdentifier:[self identifier]];
+}
+
++ (NSString *)identifier
+{
+	return NSStringFromClass([self class]);
+}
+
++ (NSString *)storyboardName
+{
+	return @"Main";
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+	[super viewWillAppear:animated];
+	[self.viewModel registerListener:self];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+	[super viewWillDisappear:animated];
+	[self.viewModel deregisterListener:self];
+}
+
+- (NSDictionary *)segueIdentifierToContainerViewControllerMapping
+{
+	return nil;
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+	[super prepareForSegue:segue sender:sender];
+	NSDictionary *identifierMapping = [self segueIdentifierToContainerViewControllerMapping];
+	
+	if (!identifierMapping)
+		return;
+	
+    NSString *path = [identifierMapping objectForKey:segue.identifier];
+    NSAssert(path, @"This segue identifier doesn't contain a mapping! Make sure segue identifier exists in the storyboard");
+    [self setValue:segue.destinationViewController forKeyPath:path];
+	if ([segue.destinationViewController isKindOfClass:[DDDViewController class]])
+	{
+		[segue.destinationViewController performSelector:@selector(setViewModel:) withObject:self.viewModel];
+	}
+}
+@end
---
+++ Hakken/Hakken/View Models/DDDViewModel.m
@@ -0,0 +1,70 @@
+//
+//  DDDViewModel.m
+//  autolayouttest
+//
+//  Created by Sidd Sathyam on 01/05/14.
+//  Copyright (c) 2014 dotdotdot. All rights reserved.
+//
+
+#import "DDDViewModel.h"
+
+@interface DDDViewModel()
+@property (strong, nonatomic) NSHashTable *listeners;
+@end
+
+@implementation DDDViewModel
+- (id)init
+{
+	self = [super init];
+	if (self)
+		self.listeners = [NSHashTable weakObjectsHashTable];
+	return self;
+}
+
+- (void)registerListener:(id<DDDViewModelListener>)listener
+{
+	if (![self.listeners containsObject:listener])
+	{
+		if(self.listeners.count == 0)
+			[self didRegisterFirstListener:listener];
+		[self.listeners addObject:listener];		
+		[self didRegisterListener:listener];
+	}
+}
+
+- (void)deregisterListener:(id<DDDViewModelListener>)listener
+{
+	if (![self.listeners containsObject:listener])
+	{
+		[self.listeners removeObject:listener];
+	}
+}
+
+- (void)didRegisterFirstListener:(id<DDDViewModelListener>)listener;
+{
+	
+}
+
+- (void)didRegisterListener:(id<DDDViewModelListener>)listener
+{
+	
+}
+
+- (void)callDelegateListenersWithSelector:(SEL)selector
+{
+	[self callDelegateListenersWithSelector:selector withObject:nil];
+}
+
+- (void)callDelegateListenersWithSelector:(SEL)selector withObject:(id)object1
+{
+	[[NSOperationQueue mainQueue] addOperationWithBlock:^{
+		for (id<DDDViewModelListener> listener in self.listeners)
+		{
+			if ([listener respondsToSelector:selector])
+			{
+				[listener performSelector:selector withObject:self withObject:object1];
+			}
+		}
+	}];
+}
+@end
