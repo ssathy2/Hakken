719784355b47d9964bf681e2a6a05152406780c1
Siddharth Sathyam <ssathy2@gmail.com>
2014-11-26 23:38:37 -0500

    Further updates to top stories

---
+++ Hakken/Hakken/Models/DDDHackerNewsItem.m
@@ -0,0 +1,25 @@
+//
+//  DDDHackerNewsItem.m
+//  Hakken
+//
+//  Created by Sidd Sathyam on 11/26/14.
+//  Copyright (c) 2014 dotdotdot. All rights reserved.
+//
+
+#import "DDDHackerNewsItem.h"
+
+@implementation DDDHackerNewsItem
+- (void)setupPropertyMappingsWithDictionary:(NSDictionary *)dictionary
+{
+    [super setupPropertyMappingsWithDictionary:dictionary];
+    [self setEnumerationMapping:@{
+                                  @"story"      : @(DDDHackerNewsItemTypeStory),
+                                  @"comment"    : @(DDDHackerNewsItemTypeComment),
+                                  @"poll"       : @(DDDHackerNewsItemTypePoll),
+                                  @"pollopt"    : @(DDDHackerNewsItemTypePollOption),
+                                  @"job"        : @(DDDHackerNewsItemTypeJob)
+                                  }
+                         forKey:@keypath(self.type)];
+}
+
+@end
