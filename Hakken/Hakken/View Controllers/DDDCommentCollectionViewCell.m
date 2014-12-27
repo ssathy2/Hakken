f7f1e529a4ad2ce9f732606d1cf1fc005a7bd817
Siddharth Sathyam <ssathy2@gmail.com>
2014-12-26 21:34:11 -0600

    Moved comment collection view cell to a separate nib

--- Hakken/Hakken/View Controllers/DDDCommentCollectionViewCell.m
+++ Hakken/Hakken/View Controllers/DDDCommentCollectionViewCell.m
@@ -10,6 +10,8 @@
 #import "DDDCommentTreeInfo.h"
 #import "DDDHackerNewsComment.h"
 
+@import ObjectiveC;
+
 @interface DDDCommentCollectionViewCell()
 @property (weak, nonatomic) IBOutlet UIView *depthIndicatorView;
 @property (weak, nonatomic) IBOutlet UILabel *commentUserLabel;
@@ -19,9 +21,51 @@
 
 @implementation DDDCommentCollectionViewCell
 
-- (void)awakeFromNib
++ (instancetype)sizingCell
 {
-    [super awakeFromNib];
+    static DDDCommentCollectionViewCell* sharedInstance;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedInstance = [[[UINib nibWithNibName:NSStringFromClass([self class]) bundle:nil] instantiateWithOwner:nil options:nil] firstObject];
+        // do any init for the shared instance here
+    });
+    return sharedInstance;
+}
+
++ (NSMutableDictionary *)sizingInfos
+{
+    static NSDictionary *dict = nil;
+    static dispatch_once_t oncePredicate;
+    dispatch_once(&oncePredicate, ^{
+        dict = [NSMutableDictionary dictionary];
+    });
+    
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wgnu"
+    return objc_getAssociatedObject(self, @selector(sizingInfos)) ?: dict;
+#pragma clang diagnostic pop
+}
+
++ (void)setSizingInfos:(NSMutableDictionary *)sizingInfos
+{
+    objc_setAssociatedObject(self, @selector(sizingInfos), sizingInfos, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
++ (CGSize)adjustedCellSizeWithModel:(id)model
+{
+    NSValue *val = [[[self class] sizingInfos] objectForKey:[model identifier]];
+    if (val)
+        return [val CGSizeValue];
+    else
+    {
+        [[self sizingCell] prepareWithModel:model];
+        [[self sizingCell] layoutIfNeeded];
+        [[self sizingCell] updateConstraintsIfNeeded];
+        
+        CGSize size = [[self sizingCell] systemLayoutSizeFittingSize:UILayoutFittingCompressedSize];
+        [[[self class] sizingInfos] setObject:[NSValue valueWithCGSize:size] forKey:[model identifier]];
+        return size;
+    }
 }
 
 - (void)prepareWithModel:(id)model
