12f2bc65ca42e5e6569e6b6ba1116f5ff490b89e
Siddharth Sathyam <ssathy2@gmail.com>
2015-01-10 11:45:46 +0530

    Adding DTCoreText and removing masonry

--- Hakken/Pods/Target Support Files/Pods/Pods-environment.h
+++ Hakken/Pods/Target Support Files/Pods/Pods-environment.h
@@ -72,11 +72,35 @@
 #define COCOAPODS_VERSION_MINOR_CocoaLumberjack_Extensions 0
 #define COCOAPODS_VERSION_PATCH_CocoaLumberjack_Extensions 0
 
-// Masonry
-#define COCOAPODS_POD_AVAILABLE_Masonry
-#define COCOAPODS_VERSION_MAJOR_Masonry 0
-#define COCOAPODS_VERSION_MINOR_Masonry 5
-#define COCOAPODS_VERSION_PATCH_Masonry 3
+// DTCoreText
+#define COCOAPODS_POD_AVAILABLE_DTCoreText
+#define COCOAPODS_VERSION_MAJOR_DTCoreText 1
+#define COCOAPODS_VERSION_MINOR_DTCoreText 6
+#define COCOAPODS_VERSION_PATCH_DTCoreText 14
+
+// DTFoundation/Core
+#define COCOAPODS_POD_AVAILABLE_DTFoundation_Core
+#define COCOAPODS_VERSION_MAJOR_DTFoundation_Core 1
+#define COCOAPODS_VERSION_MINOR_DTFoundation_Core 7
+#define COCOAPODS_VERSION_PATCH_DTFoundation_Core 4
+
+// DTFoundation/DTAnimatedGIF
+#define COCOAPODS_POD_AVAILABLE_DTFoundation_DTAnimatedGIF
+#define COCOAPODS_VERSION_MAJOR_DTFoundation_DTAnimatedGIF 1
+#define COCOAPODS_VERSION_MINOR_DTFoundation_DTAnimatedGIF 7
+#define COCOAPODS_VERSION_PATCH_DTFoundation_DTAnimatedGIF 4
+
+// DTFoundation/DTHTMLParser
+#define COCOAPODS_POD_AVAILABLE_DTFoundation_DTHTMLParser
+#define COCOAPODS_VERSION_MAJOR_DTFoundation_DTHTMLParser 1
+#define COCOAPODS_VERSION_MINOR_DTFoundation_DTHTMLParser 7
+#define COCOAPODS_VERSION_PATCH_DTFoundation_DTHTMLParser 4
+
+// DTFoundation/UIKit
+#define COCOAPODS_POD_AVAILABLE_DTFoundation_UIKit
+#define COCOAPODS_VERSION_MAJOR_DTFoundation_UIKit 1
+#define COCOAPODS_VERSION_MINOR_DTFoundation_UIKit 7
+#define COCOAPODS_VERSION_PATCH_DTFoundation_UIKit 4
 
 // ReactiveCocoa
 #define COCOAPODS_POD_AVAILABLE_ReactiveCocoa
